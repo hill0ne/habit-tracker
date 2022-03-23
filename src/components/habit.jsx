48d7f171c70212d9c3af5e0ab48ff2d41fb3a00a
hill0ne <jeanj531@gmail.com>
2022-03-23 16:40:37 +0900

    [추가] Habit 갯수 증가, 감소 기능 추가

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,20 +1,44 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
+  };
+
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
+
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
   render() {
+    const { name, count } = this.props.habit;
     return (
       <>
-        <span className="habitName">Reading</span>;
-        <span className="habitCount">8</span>;
-        <button className="habit-button habit-increase">
-          <i className="fas fa-square-plus"></i>
-        </button>
-        <button className="habit-button habit-decrease">
-          <i className="fas fa-square-minus"></i>
-        </button>
-        <button className="habit-button habit-delete">
-          <i className="fas fa-trash"></i>
-        </button>
+        <li className="habit">
+          <span className="habit-name">{name}</span>
+          <span className="habit-count">{count}</span>
+          <button
+            className="habit-button habit-increase"
+            onClick={this.handleIncrement}
+          >
+            <i className="fas fa-square-plus"></i>
+          </button>
+          <button
+            className="habit-button habit-decrease"
+            onClick={this.handleDecrement}
+          >
+            <i className="fas fa-square-minus"></i>
+          </button>
+          <button
+            className="habit-button habit-delete"
+            onClick={this.handleDelete}
+          >
+            <i className="fas fa-trash"></i>
+          </button>
+        </li>
       </>
     );
   }
